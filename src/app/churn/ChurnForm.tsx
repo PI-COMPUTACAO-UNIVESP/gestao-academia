2764ef5f1accd65319a56bb37e7340e0a293f7be
Mateus Sales Albino <mateus3009@gmail.com>
2026-05-12 22:41:07 -0300

    fix: import useState from react instead of next

--- src/app/churn/ChurnForm.tsx
+++ src/app/churn/ChurnForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'next';
+import { useState } from 'react';
 
 export default function ChurnForm() {
     const [frequenciaSemanal, setFrequenciaSemanal] = useState<number>(0);
